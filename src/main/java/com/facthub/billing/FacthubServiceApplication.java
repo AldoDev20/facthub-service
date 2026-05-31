396ed827bda4a7867e44fa0110584a0e38d51cdd
AldoDev20 <U202419485@upc.edu.pe>
2026-05-31 00:23:29 -0500

    chore: initial commit with base structure.

---
+++ src/main/java/com/facthub/billing/FacthubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.facthub.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacthubServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacthubServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/facthub/billing/FacthubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facthub.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacthubServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
