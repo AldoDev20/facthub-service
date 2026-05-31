a6b1b63442cf8849557e5692b51570ca01025a63
AldoDev20 <u202419485@upc.edu.pe>
2026-05-31 13:05:19 -0500

    style(arch): remove unused imports and rename TODOs

--- src/test/java/com/facthub/billing/archunit/ArchitectureTest.java
+++ src/test/java/com/facthub/billing/archunit/ArchitectureTest.java
@@ -1,10 +1,6 @@
 package com.facthub.billing.archunit;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
-import com.tngtech.archunit.junit.ArchTest;
-import com.tngtech.archunit.lang.ArchRule;
-
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 /**
  * Architectural tests for DDD structure.
@@ -13,13 +9,13 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 @AnalyzeClasses(packages = "com.facthub.billing")
 public class ArchitectureTest {
 
-    // TODO: Enable when controller classes are added
+    // Pending: Enable when controller classes are added
     // @ArchTest
     // static final ArchRule controllers_must_end_with_Controller =
     //         classes().that().resideInAPackage("..controller..")
     //                 .should().haveSimpleNameEndingWith("Controller");
 
-    // TODO: Enable when we fix ArchUnit classpath scanning
+    // Pending: Enable when we fix ArchUnit classpath scanning
     // @ArchTest
     // static final ArchRule dtos_must_end_with_Dto =
     //         classes().that().resideInAPackage("..dto..")
